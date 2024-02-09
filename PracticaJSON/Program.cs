601dd90e2735527c7be149a501a22c680971cc2a
CesatSoftware2 <software2@cesat.mx>
2024-02-09 10:04:45 -0800

    first commit

---
+++ PracticaJSON/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PracticaJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una lista para almacenar los taladros
+            List<Taladro> listaTaladros = new List<Taladro>();
+
+            // Crear taladros y agregarlos a la lista
+            Taladro taladro1 = new Taladro { FuerzaTorque = 50.0, RPM = 3000, Voltage = 12.0, CEnergia = 100.0 };
+            Taladro taladro2 = new Taladro { FuerzaTorque = 60.0, RPM = 4000, Voltage = 24.0, CEnergia = 150.0 };
+
+            listaTaladros.Add(taladro1);
+            listaTaladros.Add(taladro2);
+
+            // Guardar todos los taladros en un archivo JSON
+            GuardarDatosEnJSON(listaTaladros);
+
+            Console.WriteLine("Presiona cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+
+
+
+
+        static void GuardarDatosEnJSON(List<Taladro> listaTaladros)
+        {
+            // Serializar la lista de taladros a JSON
+            string jsonContent = JsonConvert.SerializeObject(listaTaladros, Formatting.Indented);
+
+            // Especifica la ruta donde deseas guardar el archivo JSON
+            string projectPath = @"C:\Users\Admin\source\repos\PracticaJSON";
+            string fileName = "taladros.json";
+            string filePath = Path.Combine(projectPath, fileName);
+
+            // Guardar el JSON en un archivo
+            File.WriteAllText(filePath, jsonContent);
+
+            Console.WriteLine($"Datos de todos los taladros guardados en el archivo JSON en: {filePath}");
+        }
+        static void ImprimirDatosTaladro(Taladro taladro)
+        {
+            Console.WriteLine($"Fuerza/Torque: {taladro.FuerzaTorque}");
+            Console.WriteLine($"RPM: {taladro.RPM}");
+            Console.WriteLine($"Voltage: {taladro.Voltage}");
+            Console.WriteLine($"C Energía: {taladro.CEnergia}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticaJSON/Taladro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaJSON
+{
+    internal class Taladro
+    {
+        // Propiedades de la clase
+        public double FuerzaTorque { get; set; }
+        public int RPM { get; set; }
+        public double Voltage { get; set; }
+        public double CEnergia { get; set; }
+        //Constructor vacio
+        public Taladro()
+        {
+
+        }
+
+        public Taladro(double fuerzaTorque, int rpm, double voltage, double cEnergia)
+        {
+            FuerzaTorque = fuerzaTorque;
+            RPM = rpm;
+            Voltage = voltage;
+            CEnergia = cEnergia;
+        }
+
+
+    }
+}
